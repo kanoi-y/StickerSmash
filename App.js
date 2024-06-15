59ec99023517f857434e92c312922c05c4e7ea4b
kanoi-y <furafuratonbo@gmail.com>
2024-06-15 21:46:48 +0900

    ✨ ジェスチャーを実装

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Button from "./components/Button";
 import CircleButton from "./components/CircleButton";
 import EmojiList from "./components/EmojiList";
@@ -48,7 +49,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
@@ -87,7 +88,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
